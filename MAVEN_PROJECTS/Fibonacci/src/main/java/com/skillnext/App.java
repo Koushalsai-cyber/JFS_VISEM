5206aef9ae1a69f08509e7b1f894f0e3f121f19f
Koushalsai-cyber <koushalkokkula@gmail.com>
2025-12-27 23:00:58 +0530

    Add Fibonacci Maven project

---
+++ MAVEN_PROJECTS/Fibonacci/src/main/java/com/skillnext/App.java
@@ -0,0 +1,37 @@
+package com.skillnext;
+
+import java.util.Scanner;
+
+public class App {
+
+    // Iterative Fibonacci (efficient)
+    public static void printFibonacci(int n) {
+        long a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            long next = a + b;
+            a = b;
+            b = next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Please enter a positive number");
+        } else {
+            printFibonacci(n);
+        }
+
+        sc.close();
+    }
+}
